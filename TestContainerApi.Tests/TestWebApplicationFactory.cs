0589b7d0f7a06fab0a77f1b94e371be9841018da
Cheranga Hatangala <cheranga@gmail.com>
2019-02-26 21:13:48 +1100

    Add project files.

---
+++ TestContainerApi.Tests/TestWebApplicationFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace TestContainerApi.Tests
+{
+    public class TestWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            //
+            // Setup the tests related implementations
+            //
+            //builder.ConfigureTestServices(services => { services.AddSingleton<ICustomerRepository, TestCustomerRepository>(); });
+        }
+    }
+}
---
+++ TestContainerApi/Controllers/TriviaController.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestContainerApi.DTO;
+
+namespace TestContainerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TriviaController : ControllerBase
+    {
+        [HttpGet("{number}")]
+        public async Task<IActionResult> Get(int number)
+        {
+            var httpClient = new HttpClient();
+            var httpResponse = await httpClient.GetAsync($"http://numbersapi.com/{number}?json");
+            var content = await httpResponse.Content.ReadAsStringAsync();
+
+            var triviaResponse = JsonConvert.DeserializeObject<TriviaResponse>(content);
+
+            return Ok(triviaResponse);
+        }
+    }
+}
